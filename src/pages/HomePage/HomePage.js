362009236eb201f8e7e75df3a878f706e5c57009
hai.lv <vanhaiit2018@gmail.com>
2019-03-08 15:14:45 +0700

    add code

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class HomePage extends Component {
     render() {
         return (
-            <div className="row">
+            <div className="row" style={{ margin: '0 auto' }}>
                 <div className="container">
-                    <h4>Home</h4>
+                    <img src="https://cdn-images-1.medium.com/max/1200/1*XpYzOtlHYkuawHTexYFyug.png" className="rounded" alt="Cinque Terre" style={{ width: '100%', height: '100%' }} />
                 </div>
             </div>
 
--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -4,7 +4,7 @@ class NotFoundPage extends Component {
     render() {
         return (
             <div className="container">
-                <h4>404</h4>
+                <img src="https://img.glyphs.co/img?src=aHR0cHM6Ly9zMy5tZWRpYWxvb3QuY29tL3Jlc291cmNlcy80MDQtRXJyb3ItUGFnZS1WZWN0b3ItSWNvbnMtUHJldmlldy0xLmpwZw&q=90&enlarge=true&h=1036&w=1600" className="rounded" alt="Cinque Terre" style={{ width: '100%', height: '600px' }} />
             </div>
 
         );
