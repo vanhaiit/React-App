1192169f4b4ee3752137b205d876b00ff0332bc5
hai.lv <vanhaiit2018@gmail.com>
2019-03-08 15:15:12 +0700

    add code new

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -8,7 +8,6 @@ class HomePage extends Component {
                     <img src="https://cdn-images-1.medium.com/max/1200/1*XpYzOtlHYkuawHTexYFyug.png" className="rounded" alt="Cinque Terre" style={{ width: '100%', height: '100%' }} />
                 </div>
             </div>
-
         );
     }
 }
